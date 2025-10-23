a7cea8c8f33fcce63b8913e685b770af68cad62a
Sebastian Morales <51178433+SebaMorales74@users.noreply.github.com>
2025-10-23 15:59:58 -0300

    FEAT: base de datos

--- bienvenida/settings.py
+++ bienvenida/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# IMPORTAR CREDENCIALES DE POSTGRESQL
+import os
+from dotenv import load_dotenv
+load_dotenv()
+#####################################
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +90,15 @@ WSGI_APPLICATION = 'bienvenida.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DBNAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
